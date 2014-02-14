a12087d4e307ad3368712af39ffd59fbe1f93435
menaerick <menaerick@hotmail.com>
2014-02-14 11:26:10 -0600

    proyecxto dia 3

---
+++ js/acciones.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function(){
+	document.addEventListener("deviceready",function(){
+		$('#B1').tap(function(){navigator.notificacion.beep(1);} );
+		
+		
+		})//device ready
+	
+	});//ready
